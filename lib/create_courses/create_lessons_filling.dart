f7fa2f26a3f5ecd880805f83cac1185bce21ac65
mgraf <mgraf276@gmail.com>
2024-09-19 14:57:11 +0300

    change of paths

--- lib/create_courses/create_lessons_filling.dart
+++ lib/create_courses/create_lessons_filling.dart
@@ -6,7 +6,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:video_player/video_player.dart' show VideoPlayerController, VideoPlayer, VideoProgressIndicator;
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';  // для определения типа контента
-import 'package:mime_type/mime_type.dart'; // для определения MIME типа файла
+import 'package:mime/mime.dart'; // для определения MIME типа файла
 
 
 class LessonData {
@@ -114,71 +114,79 @@ class _CreateLessonPage2State extends State<CreateLessonPage2> {
       headers['X-CSRFToken'] = csrfToken;
     }
 
-    final url = 'http://80.90.187.60:8001/api/mycreations/create/${widget.courseSlug}/modules/${widget.moduleId}/${widget.lessonId}/';
+    final url = 'http://80.90.187.60:8001/api/mycreations/create/${widget.courseSlug}/modules/${widget.moduleId}/${widget.lessonId}/text/';
 
-    final Map<String, dynamic> contents = {};
-
-    // Добавляем в JSON только непустые поля
-    if (_taskText.isNotEmpty) {
-      contents['text'] = {
-        "title": "Text",
-        "content": _taskText,
-      };
-    }
-
-    if (videoFile != null) {
-      contents['file'] = {
-        "title": "Video",
-        "file": videoFile!.path,
-      };
-    }
-
-    if (_testQuestion.isNotEmpty) {
-      contents['question'] = {
-        "title": "Question",
-        "text": _testQuestion,
-      };
-    }
-
-    // if (_taskText.isNotEmpty) {
-    //   contents['task'] = {
-    //     "title": "Task",
-    //     "description": "",
-    //   };
-    // }
-
-    if (_wrongAnswers.where((answer) => answer.isNotEmpty).isNotEmpty) {
-      final filteredWrongAnswers = _wrongAnswers.where((answer) => answer.isNotEmpty).map((answer) => {
-        "title": "Wrong Answer",
-        "text": answer,
-      }).toList();
-      if (filteredWrongAnswers.isNotEmpty) {
-        contents['answers'] = filteredWrongAnswers;
-      }
-    }
-
-    final Map<String, dynamic> jsonData = {
-      "contents": [contents],
+    final requestBody = {
+      'lesson_name': _lessonName,
+      'task_text': _taskText,
+      'test_question': _testQuestion,
+      'correct_answer': _correctAnswer,
+      'wrong_answers': _wrongAnswers.where((answer) => answer.isNotEmpty).toList(),
     };
-
-    print('Sending JSON data: ${jsonEncode(jsonData)}');
 
     try {
       final response = await http.post(
         Uri.parse(url),
         headers: headers,
-        body: jsonEncode(jsonData),
+        body: jsonEncode(requestBody),
       );
 
-      final rawData = utf8.decode(response.bodyBytes);
       if (response.statusCode == 200) {
-        print('Data sent successfully');
+        print('Text data sent successfully');
       } else {
-        print('Failed to send data: ${response.statusCode}');
-        print('Response body: $rawData');
+        print('Failed to send text data: ${response.statusCode}');
+        print('Response body: ${response.body}');
       }
     } catch (e) {
-      print('Error sending data: $e');
+      print('Error sending text data: $e');
+    }
+  }
+
+  Future<void> _sendVideoToServer() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final sessionId = prefs.getString('sessionid');
+    final csrfToken = prefs.getString('csrftoken');
+
+    final Map<String, String> headers = {
+      'Content-Type': 'multipart/form-data',
+    };
+
+    if (sessionId != null && csrfToken != null) {
+      headers['Cookie'] = 'sessionid=$sessionId; csrftoken=$csrfToken';
+      headers['X-CSRFToken'] = csrfToken;
+    }
+
+    final url = 'http://80.90.187.60:8001/api/mycreations/create/${widget.courseSlug}/modules/${widget.moduleId}/${widget.lessonId}/file/';
+
+    var request = http.MultipartRequest('POST', Uri.parse(url))
+      ..headers.addAll(headers);
+
+    // Если видео выбрано, добавляем его в multipart request
+    if (videoFile != null) {
+      final mimeType = lookupMimeType(videoFile!.path!);
+      final video = await http.MultipartFile.fromPath(
+        'video',
+        videoFile!.path!,
+        contentType: MediaType.parse(mimeType ?? 'video/mp4'),
+      );
+      request.files.add(video);
+
+      // Логируем информацию о видео
+      print('Отправляемое видео: ${videoFile!.name}, MIME: $mimeType');
+    }
+
+    try {
+      final streamedResponse = await request.send();
+      final response = await http.Response.fromStream(streamedResponse);
+
+      if (response.statusCode == 200) {
+        print('Video sent successfully');
+      } else {
+        print('Failed to send video: ${response.statusCode}');
+        print('Response body: ${response.body}');
+      }
+    } catch (e) {
+      print('Error sending video: $e');
     }
   }
 
@@ -305,164 +313,127 @@ class _CreateLessonPage2State extends State<CreateLessonPage2> {
                     ),
                   ),
                 ),
-              SizedBox(height: 16),
+              if (videoFile != null && (_controller == null || !_controller!.value.isInitialized))
+                Text(
+                  'Выбранное видео: ${videoFile!.name}',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Напишите текст к заданию, либо оставьте поле пустым'),
-                    SizedBox(height: 8),
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Color(0xFFF48FB1), width: 3),
+                child: Container(
+                  padding: EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF48FB1),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: TextFormField(
+                    maxLines: null,
+                    onChanged: (value) {
+                      setState(() {
+                        _taskText = value;
+                      });
+                    },
+                    style: TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                      hintText: 'Текст задания',
+                      labelStyle: TextStyle(color: Colors.white),
+                      filled: true,
+                      fillColor: Colors.white,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
                         borderRadius: BorderRadius.circular(8.0),
                       ),
-                      child: TextFormField(
-                        maxLines: null,
-                        initialValue: _taskText,
-                        onChanged: (value) {
-                          setState(() {
-                            _taskText = value;
-                          });
-                        },
-                        style: TextStyle(color: Colors.black),
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.all(8.0),
-                          hintText: 'Описание задания',
-                          border: InputBorder.none,
-                        ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(8.0),
                       ),
                     ),
-                  ],
+                  ),
                 ),
               ),
-              SizedBox(height: 16),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Введите вопрос и выберите правильный ответ'),
-                    SizedBox(height: 8),
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Color(0xFFF48FB1), width: 3),
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      child: TextFormField(
-                        initialValue: _testQuestion,
-                        onChanged: (value) {
-                          setState(() {
-                            _testQuestion = value;
-                          });
-                        },
-                        style: TextStyle(color: Colors.black),
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.all(8.0),
-                          hintText: 'Вопрос',
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    Text('Правильный ответ:'),
-                    Container(
-                      padding: EdgeInsets.all(8.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      child: TextFormField(
-                        initialValue: _correctAnswer,
-                        onChanged: (value) {
-                          setState(() {
-                            _correctAnswer = value;
-                          });
-                        },
-                        style: TextStyle(color: Colors.black),
-                        decoration: InputDecoration(
-                          hintText: 'Правильный ответ',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 16),
-                    Text('Неправильные ответы:'),
-                    SizedBox(height: 8),
-                    ReorderableListView(
-                      shrinkWrap: true,
-                      padding: EdgeInsets.zero,
-                      onReorder: (oldIndex, newIndex) {
-                        setState(() {
-                          if (newIndex > oldIndex) newIndex -= 1;
-                          final item = _wrongAnswers.removeAt(oldIndex);
-                          _wrongAnswers.insert(newIndex, item);
-                        });
-                      },
-                      children: _wrongAnswers.asMap().entries.map((entry) {
-                        int index = entry.key;
-                        String wrongAnswer = entry.value;
-                        return Padding(
-                          key: ValueKey(index),
-                          padding: const EdgeInsets.symmetric(vertical: 4.0),
-                          child: Container(
-                            padding: EdgeInsets.all(8.0),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(8.0),
-                              border: Border.all(color: Color(0xFFF48FB1)),
-                            ),
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: TextFormField(
-                                    initialValue: wrongAnswer,
-                                    onChanged: (value) {
-                                      setState(() {
-                                        _wrongAnswers[index] = value;
-                                      });
-                                    },
-                                    decoration: InputDecoration(
-                                      hintText: 'Неправильный ответ',
-                                      border: InputBorder.none,
-                                    ),
-                                  ),
-                                ),
-                                IconButton(
-                                  icon: Icon(Icons.remove, color: Colors.red),
-                                  onPressed: () => _removeWrongAnswer(index),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      }).toList(),
-                    ),
-                    SizedBox(height: 8),
-                    ElevatedButton(
-                      onPressed: _addWrongAnswer,
-                      child: Text('Добавить неправильный ответ'),
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white, backgroundColor: Color(0xFFF48FB1),
-                      ),
-                    ),
-                  ],
+                child: TextFormField(
+                  onChanged: (value) {
+                    setState(() {
+                      _testQuestion = value;
+                    });
+                  },
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    labelText: 'Вопрос для теста',
+                    labelStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(),
+                  ),
                 ),
               ),
-              SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: TextFormField(
+                  onChanged: (value) {
+                    setState(() {
+                      _correctAnswer = value;
+                    });
+                  },
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    labelText: 'Правильный ответ',
+                    labelStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: _wrongAnswers.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            onChanged: (value) {
+                              setState(() {
+                                _wrongAnswers[index] = value;
+                              });
+                            },
+                            style: TextStyle(color: Colors.black),
+                            decoration: InputDecoration(
+                              labelText: 'Неправильный ответ',
+                              labelStyle: TextStyle(color: Colors.black),
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () => _removeWrongAnswer(index),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+              ElevatedButton(
+                onPressed: _addWrongAnswer,
+                child: Text('Добавить неправильный ответ'),
+              ),
               Center(
                 child: ElevatedButton(
-                  onPressed: () {
+                  onPressed: () async {
                     if (_formKey.currentState?.validate() ?? false) {
-                      _sendTextToServer();
+                      // Сначала отправляем текстовые данные
+                      await _sendTextToServer();
+
+                      // Затем отправляем видео, если оно выбрано
+                      await _sendVideoToServer();
                     }
                   },
                   child: Text('Сохранить'),
                   style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white, backgroundColor: Color(0xFFF48FB1),
+                    foregroundColor: Colors.white,
+                    backgroundColor: Color(0xFFF48FB1),
                     padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
                     textStyle: TextStyle(fontSize: 16),
                   ),
